bc7e261266ced07bee6930c7a7cd318a03654207
Hisham Taha Kamal-Al-Din <33468983+Hisham-TK@users.noreply.github.com>
2019-03-10 10:48:01 +0200

    publish

---
+++ Python/1-8/Animals list.py
@@ -0,0 +1,9 @@
+<<<<<<< HEAD
+animals_list=lambda a:''.join(f'{i}. {v}\n' for i,v in enumerate(a))
+
+animals=['cat','dog','horse']
+
+print(animals_list(animals), '1. car\n2. dog\n3. horse\n')
+=======
+animals_list=lambda a:''.join(f'')
+>>>>>>> d6817e098f877192bbd012f82e53b6f70c4dced9
