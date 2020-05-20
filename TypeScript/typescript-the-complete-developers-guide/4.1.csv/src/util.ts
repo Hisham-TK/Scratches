e3822b07e8961094660cc72b0ce39b56f7a1d901
Hisham <hisham.taha.work@gmail.com>
2020-05-20 11:00:12 +0200

    move ts projects to examples

--- TypeScript/typescript-the-complete-developers-guide/4.1.csv/src/util.ts
+++
@@ -1,9 +0,0 @@
-export function dateStringToDate(dateString: string): Date {
-  let validDate = dateString.match(/(\d\d)\/(\d\d)\/(\d{4})/);
-  if (validDate) {
-    const [_, day, month, year] = validDate;
-    return new Date(parseInt(year), parseInt(month) - 1, parseInt(day));
-  } else {
-    throw new Error('Invalid date to parse');
-  }
-}
--- TypeScript/typescript-the-complete-developers-guide/4.2.csv/src/classes/reportTargets/ConsoleReport.class.ts
+++
@@ -1,8 +0,0 @@
-import { OutputTarget } from '../Analyze.class';
-
-export class ConsoleReport implements OutputTarget {
-  print(report: string): void {
-    // tslint:disable-next-line: no-console
-    console.log(report);
-  }
-}
